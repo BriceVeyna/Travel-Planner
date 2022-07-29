133ce06b747fcc1991509842bd649898c23ff522
hmanjun <harshith.hm03@gmail.com>
2022-07-29 12:04:04 -0700

    Add travellersRoute

---
+++ routes/api/travellersRoute.js
@@ -0,0 +1,55 @@
+const router = require('express').Router()
+const {Location, Traveller, Trips} = require('../../models')
+
+router.get('/', async (req,res) => {
+    try{
+        const travellerData = await Traveller.findAll({
+            include: [{model: Trips}]
+        })
+        res.status(200).json(travellerData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.post('/', async (req,res) => {
+    try{
+        const travellerData = await Traveller.create(req.body)
+        res.status(200).json(travellerData)
+    } catch (err){
+        res.status(400).json(err)
+    }
+})
+
+router.get('/:id', async (req,res) => {
+    try {
+        const travellerData = await Traveller.findByPK(req.params.id, {
+            include: [{model: trips}]
+        })
+        if(!travellerData) {
+            res.status(404).json({message: "No traveller found with id!"})
+            return
+        }
+        res.status(200).json(travellerData)
+    } catch {
+        res.status(500).json(err);
+    }
+})
+
+router.delete('/:id', async (req,res) => {
+    try{
+        const travellerData = await Traveller.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        if(!travellerData){
+            res.status(404).json({ message: 'No reader found with that id!' });
+            return;
+        }
+        res.status(200).json(travellerData)
+    } catch (err){
+        res.status(500).json(err)
+    }
+})
+
